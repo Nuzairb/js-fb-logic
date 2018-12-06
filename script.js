fcc047933881054f60ef85fde79055eabf119e9b
Nuzair Ibrahim <nuzairb@gmail.com>
2018-12-06 20:58:23 +0500

    init

---
+++ script.js
@@ -0,0 +1,31 @@
+var database = [
+    {
+        username: "Nuzair",
+        password: "supersecret"
+    }
+];
+
+var newsFeed = [
+    {
+        username: "Nuz",
+        timeline: "So tired from all that learning"
+    },
+    {
+        username: "Sally",
+        timeline: "JavaScript is soo cool!"
+    }
+];
+
+var userNamePrompt = prompt("What's your username?");
+var passwordPrompt = prompt("What's your Password?");
+
+function signIn(user, pass) {
+    if (user === database[0].username && 
+        pass === database[0].password) {
+            console.log(newsFeed);
+    } else {
+        alert("Sorry, wrong username and password!");
+    }
+}
+
+signIn(userNamePrompt, passwordPrompt);
