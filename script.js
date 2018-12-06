88cd4094db00d86021dd98aac86a919c683d822f
Nuzair Ibrahim <nuzairb@gmail.com>
2018-12-06 19:49:54 +0500

    finished funtion exercise

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
-function multiply(a, b) {
-        return a * b;
+// var age = prompt("What is your age?");
+
+// if (Number(age) < 18) {
+//     alert("Sorry, you are too yound to drive this car. Powering off");
+// } else if (Number(age) > 18) {
+//     alert("Powering On. Enjoy the ride!");
+// } else if (Number(age) === 18) {
+//     alert("Congratulations on your first year of driving. Enjoy the ride!");
+// }
+
+// function checkDriverAge() {
+//     var age = prompt("What is your age?");
+//     if (Number(age) < 18) {
+//         alert("Sorry, you are too yound to drive this car. Powering off");
+//     } else if (Number(age) > 18) {
+//         alert("Powering On. Enjoy the ride!");
+//     } else if (Number(age) === 18) {
+//         alert("Congratulations on your first year of driving. Enjoy the ride!");
+//     }
+// }
+
+function checkDriverAge (age) {
+    var age = prompt("What is your age?");
+    if (Number(age) < 18) {
+        return "Sorry, you are too yound to drive this car. Powering off";
+    } else if (Number(age) > 18) {
+        return "Powering On. Enjoy the ride!";
+    } else if (Number(age) === 18) {
+        return "Congratulations on your first year of driving. Enjoy the ride!";
+    }
 }
 
-alert(multiply(5, 10)); 
+checkDriverAge(92);
