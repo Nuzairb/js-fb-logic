fa1017f67947283fcaeafecdb4be3c36ea3a0072
Nuzair Ibrahim <nuzairb@gmail.com>
2018-12-07 19:18:22 +0500

    init

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ var database = [
     {
         username: "Nuzair",
         password: "supersecret"
+    }, 
+    {
+        username: "sally",
+        password: "123"
+    },
+    {
+        username: "ingrid",
+        password: "777"
     }
 ];
 
@@ -16,57 +24,18 @@ var newsFeed = [
     }
 ];
 
-var userNamePrompt = prompt("What's your username?");
-var passwordPrompt = prompt("What's your Password?");
 
-function signIn(user, pass) {
-    if (user === database[0].username && 
-        pass === database[0].password) {
+
+function signIn(username, password) {
+    if (username === database[0].username && 
+        password === database[0].password) {
             console.log(newsFeed);
     } else {
         alert("Sorry, wrong username and password!");
     }
 }
 
+var userNamePrompt = prompt("What's your username?");
+var passwordPrompt = prompt("What's your Password?");
+
 signIn(userNamePrompt, passwordPrompt);
-
-var todos = [
-    "clean room",
-    "brush teeth",
-    "exercise",
-    "study javascript",
-    "eat healthy"
-];
-
-var todosImportant = [
-    "clean room!",
-    "brush teeth!",
-    "exercise!",
-    "study javascript!",
-    "eat healthy!"
-];
-
-var todosLength = todos.length;
-
-for (var i=0; i < todosLength; i++) {
-    console.log(i);
-}
-
-function logTodos(todo, i) {
-    console.log(todo, i);
-}
-
-todos.forEach(logTodos);
-todosImportant.forEach(logTodos);
-
-// var counterOne = 10;
-// while (counterOne > 0) {
-//     console.log(counterOne);
-//     counterOne--
-// }
-
-// var counterTwo = 10;
-// do {
-//     console.log(counterTwo);
-//     counterTwo--;
-// } while (counterTwo > 0);
