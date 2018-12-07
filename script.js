7440c309a76f0fd86dfd8a21826e7f44a75d8143
Nuzair Ibrahim <nuzairb@gmail.com>
2018-12-07 12:05:32 +0500

    init

--- script.js
+++ script.js
@@ -38,14 +38,35 @@ var todos = [
     "eat healthy"
 ];
 
+var todosImportant = [
+    "clean room!",
+    "brush teeth!",
+    "exercise!",
+    "study javascript!",
+    "eat healthy!"
+];
+
 var todosLength = todos.length;
 
 for (var i=0; i < todosLength; i++) {
-    todos.pop();
+    console.log(i);
 }
 
-var counterOne = 10;
-while (counterOne > 0) {
-    console.log(counterOne);
-    counterOne--
+function logTodos(todo, i) {
+    console.log(todo, i);
 }
+
+todos.forEach(logTodos);
+todosImportant.forEach(logTodos);
+
+// var counterOne = 10;
+// while (counterOne > 0) {
+//     console.log(counterOne);
+//     counterOne--
+// }
+
+// var counterTwo = 10;
+// do {
+//     console.log(counterTwo);
+//     counterTwo--;
+// } while (counterTwo > 0);
