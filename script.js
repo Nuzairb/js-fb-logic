cffe35e64b07114b8b92aaf9dbf1628cd24801a2
Nuzair Ibrahim <nuzairb@gmail.com>
2018-12-07 11:54:09 +0500

    while loop

--- script.js
+++ script.js
@@ -38,6 +38,14 @@ var todos = [
     "eat healthy"
 ];
 
-for (var i=0; i < todos.length; i++) {
-    console.log(todos[i] + "!");
+var todosLength = todos.length;
+
+for (var i=0; i < todosLength; i++) {
+    todos.pop();
+}
+
+var counterOne = 10;
+while (counterOne > 0) {
+    console.log(counterOne);
+    counterOne--
 }
